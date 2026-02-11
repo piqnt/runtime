bf9b9c965c867fa29d972edd440ce194bb733ea4
Ali Shakiba <shakiba@users.noreply.github.com>
2026-02-11 19:56:21 +0330

    Export middlewares

---
+++ .changeset/gold-cougars-post.md
@@ -0,0 +1,5 @@
+---
+"planck-testbed": patch
+---
+
+Export middlewares
--- packages/planck-testbed/src/index.ts
+++ packages/planck-testbed/src/index.ts
@@ -1,1 +1,21 @@
+export * from "./common/Magicboard";
+export * from "./testbed/Gamepad";
+export * from "./testbed/ConsolePlugin";
+export * from "./world-view";
+export * from "./world-view/WorldPointer";
+export * from "./common/StageLoader";
+export * from "./common/ZoomPan";
+
+export * from "./world-play/ImpulseTool";
+export * from "./world-play/PullTool";
+
+export * from "./common-tool";
+
+export * from "./testbed/TestbedContext";
+export * from "./testbed/Testbed";
+export * from "./common/ContainerLoader";
+export * from "./common/FrameLoop";
+export * from "./world-view/WorldStep";
+export * from "./world-play/TestbedTool";
+
 export * from "./testbed/runtime";
